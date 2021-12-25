3a25fa7f600c8b96fe6b929891767b5f8e9c0327
Parth Godhani <pg.godhani@gmail.com>
2021-12-25 16:06:46 +0530

    web3 laoded and account intialize

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,24 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out parth godhani");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
+
 }
